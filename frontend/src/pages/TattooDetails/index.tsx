452f3208405f7e0b398406bef0b099fad5e8ac0e
Filipe Lima <fhemrique55@gmail.com>
2024-03-23 16:55:56 -0300

    ajustes

--- frontend/src/pages/TattooDetails/index.tsx
+++ frontend/src/pages/TattooDetails/index.tsx
@@ -3,8 +3,22 @@ import "./styles.css";
 import tattoimage from "../../assets/img/tatuagem-tatuador.png";
 import artist from "../../assets/img/tatuador.jpeg";
 import { formatPrice } from "../../utils/formaters";
+import { Tattoo } from "../../types/Tattoo";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { BASE_URL } from "../../utils/request";
 
 const TattooDetails = () => {
+
+  const [tattoo, setTattoo] = useState<Tattoo>();
+
+  useEffect(() =>{
+   axios.get(BASE_URL + "/1")
+   .then(response => {
+    setTattoo(response.data)
+   });
+  }, [])
+
   return (
     <div className="product-details-container my-5">
       <div className="goback-container">
@@ -15,33 +29,33 @@ const TattooDetails = () => {
       <div className="tattoo-card-container">
         <div className="row card-details-container">
           <div className="col-xl-6 card-tattoo-details-image">
-            <img src={tattoimage} alt="" />
-            <h3>nome tatuagem</h3>
+            <img src={tattoo?.artUrl} alt="" />
+            <h3>{tattoo?.name}</h3>
           </div>
           <div className="col-xl-4 card-tattoo-details-resume">
             <div className="artist-resume-detais">
               <div className="img-artist-resume">
-                <img src={artist} />
-                <span>nome do artista</span>
+                <img src={tattoo?.artist.photo} />
+                <span>{tattoo?.artist.name}</span>
               </div>
               <p>
-                perfil: <span>nome do artista</span>
+                perfil: <span>{tattoo?.artist.perfil}</span>
               </p>
               <p>
-                contact: <span>81 932324345</span>
+                contact: <span>{tattoo?.artist.contact}</span>
               </p>
               <p>
-                email: <span>artista@gmail.com</span>
+                email: <span>{tattoo?.artist.email}</span>
               </p>
             </div>
 
             <div className="tatto-description-details">
               <p>
-                STYLE: <span>estilo</span>
+                STYLE: <span>{tattoo?.style.name}</span>
               </p>
-              <text>Tradicional americana arte</text>
+              <text>DESCRIÇÃO: <span>{tattoo?.description}</span></text>
 
-              <p className="price-tattoo-details">PREÇO: {formatPrice(200)}</p>
+              <p className="price-tattoo-details">PREÇO: {tattoo?.price == undefined ? 'VAZIO' : formatPrice(tattoo.price)}</p>
             </div>
           </div>
         </div>
