8fd7ad5609f5927966667eb1f2a0ea5563b6f24a
Filipe Lima <fhemrique55@gmail.com>
2024-05-01 00:09:03 -0300

    Feat: Estilizando margens e containers do formulário

--- frontend/src/pages/Admin/Tattoos/Form/index.tsx
+++ frontend/src/pages/Admin/Tattoos/Form/index.tsx
@@ -7,18 +7,29 @@ const Form = () => {
         <h1 className="tattoo-crud-form-title">Dados da Tatuagem</h1>
 
         <form action="">
-          <div className="row">
-            <div className="col-lg-6">
-              <input className="form-control base-input" type="text" />
-              <input className="form-control base-input" type="text" />
-              <input className="form-control base-input" type="text" />
+          <div className="row tattoo-crud-inputs-container">
+            <div className="col-lg-6 tattoo-crud-inputs-left">
+              <div className="margin-bottom-30">
+                <input className="form-control base-input" type="text" />
+              </div>
+              <div className="margin-bottom-30">
+                <input className="form-control base-input" type="text" />
+              </div>
+
+              <div>
+                <input className="form-control base-input" type="text" />
+              </div>
             </div>
             <div className="col-lg-6">
-              <textarea className="form-control base-input" name="" rows={10}></textarea>
+              <textarea
+                className="form-control base-input"
+                name=""
+                rows={10}
+              ></textarea>
             </div>
           </div>
 
-          <div>
+          <div className="tattoo-crud-buttons-container">
             <button className="btn btn-outline-danger">CANCELAR</button>
             <button className="btn btn-primary">SALVAR</button>
           </div>
