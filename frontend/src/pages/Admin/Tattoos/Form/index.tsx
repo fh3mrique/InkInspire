015cfe7e5690bd9345ef8e83dfe5b0e52cd1ce10
Filipe Lima <fhemrique55@gmail.com>
2024-04-30 23:53:36 -0300

    Feat:  Começando o HTML do formulário de produtos

--- frontend/src/pages/Admin/Tattoos/Form/index.tsx
+++ frontend/src/pages/Admin/Tattoos/Form/index.tsx
@@ -1,5 +1,31 @@
+import "./styles.css";
+
 const Form = () => {
-  return <div>Form para cadastrar a tatoo</div>;
+  return (
+    <div className="tattoo-crud-container">
+      <div className=" tattoo-crud-form-card">
+        <h1 className="tattoo-crud-form-title">Dados da Tatuagem</h1>
+
+        <form action="">
+          <div className="row">
+            <div className="col-lg-6">
+              <input className="form-control base-input" type="text" />
+              <input className="form-control base-input" type="text" />
+              <input className="form-control base-input" type="text" />
+            </div>
+            <div className="col-lg-6">
+              <textarea className="form-control base-input" name="" rows={10}></textarea>
+            </div>
+          </div>
+
+          <div>
+            <button className="btn btn-outline-danger">CANCELAR</button>
+            <button className="btn btn-primary">SALVAR</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Form;
