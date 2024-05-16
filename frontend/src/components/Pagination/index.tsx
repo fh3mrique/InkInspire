39b5c38ba9d9707e77311f1ca0f7e627d53aaf31
Filipe Lima <fhemrique55@gmail.com>
2024-05-16 01:22:14 -0300

    Pagination layout

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -1,23 +1,32 @@
 import "./styles.css";
 import ArrowRight from "../../assets/img/arrow-back.svg";
+import ReactPaginate from "react-paginate";
 const Pagination = () => {
   return (
-    <div className="pagination-container">
-      <div className="previouspage arrow">
-        <img className="arrow-left" src={ArrowRight} alt="" />
-        <span className="arrow-text">Previous</span>
-      </div>
+    <>
+      <ReactPaginate 
+      pageCount={10} 
+      pageRangeDisplayed={3}
+      marginPagesDisplayed={1}
+       />
 
-      <div className="pagination-number">1</div>
-      <div className="pagination-number">2</div>
-      <div className="pagination-number pagination-active">3</div>
-      <div className="pagination-number">4</div>
-      <div className="pagination-number">540</div>
+      <div className="pagination-container">
+        <div className="previouspage arrow">
+          <img className="arrow-left" src={ArrowRight} alt="" />
+          <span className="arrow-text">Previous</span>
+        </div>
 
-      <div className="pagination-number arrow">
-        <img className="arrow-left" src={ArrowRight} alt="" />
+        <div className="pagination-number">1</div>
+        <div className="pagination-number">2</div>
+        <div className="pagination-number pagination-active">3</div>
+        <div className="pagination-number">4</div>
+        <div className="pagination-number">540</div>
+
+        <div className="pagination-number arrow">
+          <img className="arrow-left" src={ArrowRight} alt="" />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
