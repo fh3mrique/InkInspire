6534bd2576fd1e65649dd58835dfaa71beb9022f
Filipe Lima <fhemrique55@gmail.com>
2024-05-17 08:52:49 -0300

    Feat: Layout do input name

--- frontend/src/components/TattooFilter/index.tsx
+++ frontend/src/components/TattooFilter/index.tsx
@@ -1,11 +1,17 @@
 import "./styles.css";
+import SearchIcon from "../../assets/img/search-icon.svg";
 
 const TattooFilter = () => {
   return (
     <div className="base-card tatoo-filter-container">
       <form action="" className="tattoo-filter-form">
         <div className="tattoo-filter-name-container">
-          <input type="text" className="form-control" />
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Nome da tatuagem"
+          />
+          <img src={SearchIcon} alt="" />
         </div>
 
         <div className="tattoo-filter-bottom-container">
